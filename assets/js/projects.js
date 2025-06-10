11d168661b1c3c865c7001bd18b8541b3baaea3d
kkdonjuun122 <151377183+kkdonjuun122@users.noreply.github.com>
2025-06-10 19:51:07 +0900

    Add files via upload

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,113 +12,113 @@ let render_projects = (slug) => {
     let projects_obj = [
         {
             image: 'assets/images/mentors.jpg',
-            title: 'Mporter',
-            demo: 'https://mporter.co',
+            title: '턴제 게임',
+            // //demo : 'https://mporter.co',
             description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
-            categories: ['featured', 'game']
+            categories: ['game']
         },
         {
             image: 'assets/images/mobile-landscape.jpg',
-            title: 'Wall-E',
-            demo: 'http://wall-e-jekyll.github.io/',
+            title: '보안 위협 요소 찾기',
+            ////demo : 'http://wall-e-jekyll.github.io/',
             description: "A modern Jekyll theme with grid frontpage, beautiful typography, mobile responsive, made with Semantic UI.",
-            categories: ['featured', 'game']
+            categories: ['game']
         },
         {
             image: 'assets/images/collage.jpg',
-            title: 'Marvel',
-            demo: false,
+            title: '테스트',
+            //demo : false,
             description: "Attendance marking tool that uses face recognition for marking attendance and firebase for tracking and analytics.",
-            categories: ['featured', 'native']
+            categories: ['security']
         },
         {
             image: 'assets/images/mpw.jpg',
-            title: 'Master Password',
-            demo: 'https://www.nagekar.com/mpw',
+            title: '테스트123',
+            //demo : 'https://www.nagekar.com/mpw',
             description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
-            categories: ['featured', 'security']
+            categories: ['security']
         },
         {
             image: 'assets/images/social-share-count.jpeg',
             title: 'Social Share Count',
-            demo: false,
+            //demo : false,
             description: "Ever wondered how many times a URL has been shared on popular social networks?",
-            categories: ['native']
+            categories: ['security']
         },
         {
             image: 'assets/images/data-destroyer.png',
             title: 'Data Destroyer',
-            demo: false,
+            //demo : false,
             description: "Native GUI wrapper for GNU coreutils tool 'dd'",
-            categories: ['native']
+            categories: ['game']
         },
         {
             image: 'assets/images/raspberry-pi-monitor.png',
             title: 'Raspberry Pi Monitor',
-            demo: false,
+            //demo : false,
             description: "Web based status monitor/smart mirror, displays system stats, weather and more.",
-            categories: ['game', 'diy']
+            categories: ['game']
         },
         {
             image: 'assets/images/s3scan.png',
             title: 'S3Scan',
-            demo: false,
+            //demo : false,
             description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
-            categories: ['native', 'security']
+            categories: ['security']
         },
         {
             image: 'assets/images/elementary.png',
             title: 'Elementary',
-            demo: 'https://elementary-jekyll.github.io/',
+            //demo : 'https://elementary-jekyll.github.io/',
             description: "Elementary is a zero Javascript and minimal CSS ultra lightweight Jekyll theme for those of you who love simplicity.",
             categories: ['game']
         },
         {
             image: 'assets/images/soot-spirits.png',
             title: 'Soot Spirits',
-            demo: 'https://sootspirits.github.io',
+            //demo : 'https://sootspirits.github.io',
             description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
             categories: ['game']
         },
         {
             image: 'assets/images/python-chat.png',
             title: 'Terminal Group Chat',
-            demo: false,
+            //demo : false,
             description: "Simple terminal group chat based on native sockets using Python.",
             categories: ['native']
         },
         {
             image: 'assets/images/old-lcd.jpg',
             title: 'Reusing Old LCD Panel',
-            demo: false,
+            //demo : false,
             description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
             categories: ['diy']
         },
         {
             image: 'assets/images/nextcloud-enc.png',
             title: 'Encrypted Self-Hosted Cloud',
-            demo: false,    
+            //demo : false,    
             description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
             categories: ['diy', 'security']
         },
         {
             image: 'assets/images/google-cloud-backup.png',
             title: 'Encrypted Backups - Google Cloud',
-            demo: false,
+            //demo : false,
             description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
             categories: ['diy', 'security']
         },
         {
             image: 'assets/images/pi-cloud.jpg',
             title: 'Local Cloud - Raspberry Pi',
-            demo: false,
+            //demo : false,
             description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
             categories: ['diy']
         },
         {
             image: 'assets/images/koalamate.png',
             title: 'Koalamate',
-            demo: false,
+            //demo : false,
             description: "A cross-platform desktop application that serves as a Wolfram Alpha query place and notes taker.",
             categories: ['native']
         },
