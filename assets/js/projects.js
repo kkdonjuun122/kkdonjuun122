cf56f24d98c0d03ce2dcb5a3ffa425a2ab456128
kkdonjuun122 <151377183+kkdonjuun122@users.noreply.github.com>
2025-06-11 19:58:07 +0900

    Add files via upload

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,119 +12,39 @@ let render_projects = (slug) => {
     let projects_obj = [
         {
             image: 'assets/images/Turn_game.png',
-            title: '턴제 게임',
+            title: '턴제 게임 (예정)',
             // //demo : 'https://mporter.co',
             description: "말 그대로 턴제 게임입니다. 서로 차례를 주고 받으면서 행동을 수행합니다.",
             categories: ['favorites','game']
         },
         {
             image: 'assets/images/Security_guard2.png.png',
-            title: '보안 위협 요소 찾기',
+            title: '보안 위협 요소 찾기 (예정) ',
             ////demo : 'http://wall-e-jekyll.github.io/',
             description: "게임 내에서 컴퓨터를 조작하며 바이러스가 침투할 수 있는 요소를 찾아 제거하는 게임입니다.",
             categories: ['game','security']
         },
         {
             image: 'assets/images/Security_Tool.png.png',
-            title: '보안 툴 만들기',
+            title: '보안 툴 만들기 (예정)',
             //demo : false,
             description: "저만의 간단한 보안 툴을 만들어 보는 것입니다.",
             categories: ['favorites','security']
         },
         {
             image: 'assets/images/White_Black.png',
-            title: '화이트 해커 & 블랙 해커',
+            title: '화이트 해커 & 블랙 해커 (예정)',
             //demo : 'https://www.nagekar.com/mpw',
-            description: "이 둘의 개념을 확실하게 배워 보고 싶습니다다.",
-            categories: ['security']
-            
-        },
-        /* 사용 안하는 애들, 일단 남겨둠
-        {
-            image: 'assets/images/social-share-count.jpeg',
-            title: 'Social Share Count',
-            //demo : false,
-            description: "Ever wondered how many times a URL has been shared on popular social networks?",
+            description: "이 둘의 개념을 확실하게 배워 보고 싶습니다.",
             categories: ['security']
         },
         {
-            image: 'assets/images/data-destroyer.png',
-            title: 'Data Destroyer',
+            image: 'assets/images/Unity.png',
+            title: 'Unity 기반 모바일 게임 만들기 (예정)',
             //demo : false,
-            description: "Native GUI wrapper for GNU coreutils tool 'dd'",
-            categories: ['game']
+            description: "Unity를 사용하여 모바일 게임을 만들고 싶습니다다.",
+            categories: ['game','favorites']
         },
-        {
-            image: 'assets/images/raspberry-pi-monitor.png',
-            title: 'Raspberry Pi Monitor',
-            //demo : false,
-            description: "Web based status monitor/smart mirror, displays system stats, weather and more.",
-            categories: ['game']
-        },
-        {
-            image: 'assets/images/s3scan.png',
-            title: 'S3Scan',
-            //demo : false,
-            description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
-            categories: ['security']
-        },
-        {
-            image: 'assets/images/elementary.png',
-            title: 'Elementary',
-            //demo : 'https://elementary-jekyll.github.io/',
-            description: "Elementary is a zero Javascript and minimal CSS ultra lightweight Jekyll theme for those of you who love simplicity.",
-            categories: ['game']
-        },
-        {
-            image: 'assets/images/soot-spirits.png',
-            title: 'Soot Spirits',
-            //demo : 'https://sootspirits.github.io',
-            description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
-            categories: ['game']
-        },
-        {
-            image: 'assets/images/python-chat.png',
-            title: 'Terminal Group Chat',
-            //demo : false,
-            description: "Simple terminal group chat based on native sockets using Python.",
-            categories: ['native']
-        },
-        {
-            image: 'assets/images/old-lcd.jpg',
-            title: 'Reusing Old LCD Panel',
-            //demo : false,
-            description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/nextcloud-enc.png',
-            title: 'Encrypted Self-Hosted Cloud',
-            //demo : false,    
-            description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/google-cloud-backup.png',
-            title: 'Encrypted Backups - Google Cloud',
-            //demo : false,
-            description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/pi-cloud.jpg',
-            title: 'Local Cloud - Raspberry Pi',
-            //demo : false,
-            description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/koalamate.png',
-            title: 'Koalamate',
-            //demo : false,
-            description: "A cross-platform desktop application that serves as a Wolfram Alpha query place and notes taker.",
-            categories: ['native']
-        },
-        */
     ]
 
     let projects = [];
